67cb2b11b2116a5676405df4363ab4fbeb2e864b
Juan Carlos Orantes <72782842+Orantes-J@users.noreply.github.com>
2022-04-29 15:13:14 -0700

    new list of quotes

--- src/js/quotes.js
+++ src/js/quotes.js
@@ -25,3 +25,17 @@ export const curiousQuotes = [
     {"quote" : "The best way out is always through.", "author" : "Robert Frost"},
     {"quote" : "I destroy my enemies when I make them my friends.", "author" : "Abraham Lincoln"},
 ];
+
+export const wiseQuotes = [
+    {"quote" : "A clever person solves a problem. A wise person avoids it.", "author" : "Albert Einstein"},
+    {"quote" : "The wise person feels the pain of one arrow. The unwise feels the pain of two.", "author" : "Kate Carne"},
+    {"quote" : "You've heard that it's wise to learn from experience, but it is wiser to learn from the experience of others.", "author" : "Rick Warren"},
+    {"quote" : "Some of us think holding on makes us strong, but sometimes it is letting go.", "author" : "Hermann Hesse"},
+    {"quote" : "Don't waste your time with explanations, people only hear what they want to hear.", "author" : "Paulo Coelho"},
+    {"quote" : "Pain can change you, but that doesn't mean it has to be a bad change. Take that pain and turn it into wisdom.", "author" : "Dalai Lama"},
+    {"quote" : "Faith in God includes faith in God's timing.", "author" : "Neal A. Maxwell"},
+    {"quote" : "If we manage ego wisely, we get the upside it delivers followed by strong returns", "author" : "dave Marcum"},
+    {"quote" : "Be wiser than other people, if you can; but do not tell them so.", "author" : "Lord Chesterfield"},
+    {"quote" : "Knowledge is the crown of the wise, folly is an inheritance of a fool.", "author" : "Vusi Mxolisi Zitha"},
+];
+    
