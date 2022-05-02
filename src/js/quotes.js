f666732f276bd4d5ff3fa744d0c2a8c67934d7ea
Juan Carlos Orantes <72782842+Orantes-J@users.noreply.github.com>
2022-05-01 17:53:24 -0700

    commit 2

---
+++ src/js/quotes.js
@@ -0,0 +1,109 @@
+'use strict';
+
+export const happyQuotes = [
+    [
+        {"quote" : "There is no path to happiness; happiness is the path.", "author" : "Buddha"},
+        {"quote" : "Don't worry. Be happy.", "author" : "Bobby McFerrin"},
+        {"quote" : "I have chosen to be happy because it's good for my health.", "author" : "Voltaire"},
+        {"quote" : "Happiness is a journey, not a destination.", "author" : "Buddha"},
+        {"quote" : "No medicine cures what happiness cannot.", "author" : "Gabriel García Márquez"},
+        {"quote" : "They say a person needs just three things to be truly happy in this world: Someone to love, something to do, and something to hope for.", "author" : "Tom Bodett"},
+        {"quote" : "Let us be grateful to the people who make us happy; they are the charming gardeners who make our souls blossom.", "author" : "Marcel Proust"},
+        {"quote" : "Happiness is a warm puppy.", "author" : "Charles M. Schulz"},
+        {"quote" : "There's nothing like deep breaths after laughing that hard. Nothing in the world like a sore stomach for the right reasons.", "author" : "Stephen Chbosky"},
+        {"quote" : "Sanity and happiness are an impossible combination.", "author" : "Mark Twain"},
+    ],
+    [
+        {"quote" : "You cannot protect yourself from sadness without protecting yourself from happiness.", "author" : "Jonathan Safran Foer"},
+        {"quote" : "It's so hard to forget pain, but it's even harder to remember sweetness. We have no scar to show for happiness. We learn so little from peace.", "author" : "Chuck Palahniuk"},
+        {"quote" : "If you want to be happy, do not dwell in the past, do not worry about the future, focus on living fully in the present.", "author" : "Roy T. Bennett"},
+        {"quote" : "It isn't what you have or who you are or where you are or what you are doing that makes you happy or unhappy. It is what you think about it.", "author" : "Dale Carnegie"},
+        {"quote" : "Happiness is a state of mind. It's just according to the way you look at things.", "author" : "Walt Disney"},
+        {"quote" : "Think of all the beauty still left around you and be happy.", "author" : "Anne Frank"},
+        {"quote" : "Happiness is not a goal...it's a by-product of a life well-lived.", "author" : "Eleanor Roosevelt"},
+        {"quote" : "The best way to cheer yourself is to try to cheer someone else up.", "author" : "Mark Twain"},
+        {"quote" : "Don't cry because it's over, smile because it happened.", "author" : "Dr. Seuss"},
+        {"quote" : "Happiness depends upon ourselves.", "author" : "Aristotle"},
+    ]
+]
+
+export const downQuotes = [
+    [   
+        {"quote" : "Anticipate the difficult by managing the easy.", "author" : "Lao Tzu"},
+        {"quote" : "I believe that every person is born with talent.", "author" : "Maya Angelou"},
+        {"quote" : "Our distrust is very expensive.", "author" : "Ralph Emerson"},
+        {"quote" : "I never think of the future. It comes soon enough", "author" : "Albert Einstein"},
+        {"quote" : "In order to win, you must expect to win.", "author" : "Richard Bach"},
+        {"quote" : "Some pursue happiness, others create it.", "author" : "null"},
+        {"quote" : "Don't ruin the present with the ruined past.", "author" : "Ellen Gilchrist"},
+        {"quote" : "What we think, we become.", "author" : "Buddha"},
+        {"quote" : "The shortest answer is doing.", "author" : "Lord Herbert"},
+        {"quote" : "Our intention creates our reality.", "author" : "Wayne Dyer"},
+    ],
+    [
+        {"quote" : "Not all who wonder are lost.", "author" : "null"},
+        {"quote" : "You know it was a good day if you didn't hit or bite anybody.", "author" : "Nathaniel Parizek"},
+        {"quote" : "Today I will be happier than a bird with a french fry. ", "author" : "null"},
+        {"quote" : "What we see depends mainly on what we look for.", "author" : "John Lubbock"},
+        {"quote" : "We must always change, renew, rejuevenate ourselves. Otherwise we harden.", "author" : "Goethe"},
+        {"quote" : "Consider the postage stamp: its usefulness consists in the ability to stick to one thing 'til it gets there.", "author" : "Josh Billings"},
+        {"quote" : "I always wanted to be somebody, but now I realize I should have been more specific.", "author" : "Lily Tomlin"},
+        {"quote" : "If you're going to be able to look back on something and laugh about it, you might as well laugh about it now.", "author" : "Marie Osmond"},
+        {"quote" : "Here is a test to find whether your mission on earth is finished: If you're alive it isn't.", "author" : "Richard Bach"},
+        {"quote" : "There is nothing in a caterpillar that tells you it's going to be a butterfly.", "author" : "Buckminster Fuller"},
+    ]
+];
+
+export const curiousQuotes = [
+    [
+        {"quote" : "Speak low, if you speak love.", "author" : "William Shakespeare"},
+        {"quote" : "The greatest remedy for anger is delay.", "author" : "Seneca"},
+        {"quote" : "Wherever you go, go with all your heart.", "author" : "Confucius"},
+        {"quote" : "If the shoe doesn't fit, must we change the foot?", "author" : "Gloria Steinem"},
+        {"quote" : "While we stop to think, we often miss our opportunity.", "author" : "Publilius Syrus"},
+        {"quote" : "Life isn't about finding yourself. Life is about creating yourself.", "author" : "Bernard Shaw"},
+        {"quote" : "Once we accept our limits, we go beyond them.", "author" : "Albert Einstein"},
+        {"quote" : "Never ignore a gut feeling, but never believe that it's enough.", "author" : "Robert Heller"},
+        {"quote" : "The best way out is always through.", "author" : "Robert Frost"},
+        {"quote" : "I destroy my enemies when I make them my friends.", "author" : "Abraham Lincoln"},
+    ],
+    [
+        {"quote" : "The wisest man is he who does not believe he is wise.", "author" : "Proverb Believe"},
+        {"quote" : "The greatest chapters in history always begin with risk, and the same is true with the chapters of your life.", "author" : "Mark Batterson"},
+        {"quote" : "The wise does at once what the fool does at last.", "author" : "Baltstar Gracian"},
+        {"quote" : "A man without dignity is a man without hope.", "author" : "Andreas Chrysaphes"},
+        {"quote" : "The heart can create troubles for the head—the heart knows nothing of logic.", "author" : "Osho Intution"},
+        {"quote" : "Life isn't about finding yourself. Life is about creating yourself.", "author" : "Bernard Shaw"},
+        {"quote" : "Some people are so poor, all they have is money.", "author" : "Jack Kerouac"},
+        {"quote" : "He who has a why to live can bear almost any how.", "author" : "Friedrich Nietzsche"},
+        {"quote" : "Take into account that great love and great achievements involve great risk.", "author" : "Dalai Lama"},
+        {"quote" : "Lost time is never found again.", "author" : "Benjamin Franklin"},
+    ]
+];
+
+export const wiseQuotes = [
+    [
+        {"quote" : "A clever person solves a problem. A wise person avoids it.", "author" : "Albert Einstein"},
+        {"quote" : "The wise person feels the pain of one arrow. The unwise feels the pain of two.", "author" : "Kate Carne"},
+        {"quote" : "You've heard that it's wise to learn from experience, but it is wiser to learn from the experience of others.", "author" : "Rick Warren"},
+        {"quote" : "Some of us think holding on makes us strong, but sometimes it is letting go.", "author" : "Hermann Hesse"},
+        {"quote" : "Don't waste your time with explanations, people only hear what they want to hear.", "author" : "Paulo Coelho"},
+        {"quote" : "Pain can change you, but that doesn't mean it has to be a bad change. Take that pain and turn it into wisdom.", "author" : "Dalai Lama"},
+        {"quote" : "Faith in God includes faith in God's timing.", "author" : "Neal A. Maxwell"},
+        {"quote" : "If we manage ego wisely, we get the upside it delivers followed by strong returns", "author" : "dave Marcum"},
+        {"quote" : "Be wiser than other people, if you can; but do not tell them so.", "author" : "Lucius"},
+        {"quote" : "Using pride wisely requires a solid grasp of when and why it can go wrong.", "author" : "David DeSteno"},
+    ],
+    [
+        {"quote" : "Knowledge is the crown of the wise, folly is an inheritance of a fool.", "author" : "Vusi Zitha"},
+        {"quote" : "The nature of life is such that the fool does not always fail, nor is the wise man always successful.", "author" : "Janice Manning"},
+        {"quote" : "Only the wisest and stupidest of men never change.", "author" : "Confucius"},
+        {"quote" : "Do not be wise in words be wise in deeds.", "author" : "Jewish Proverb"},
+        {"quote" : "The simple things are also the most extraordinary things, and only the wise can see them.", "author" : "Paulo Coelho"},
+        {"quote" : "A wise man can learn more from a foolish question than a fool can learn from a wise answer.", "author" : "Bruce Lee"},
+        {"quote" : "We never have more than we can bear. The present hour we are always able to endure.", "author" : "H.E Manning"},
+        {"quote" : "The wise lack for nothing. The foolish, meanwhile, cannot make good use of anything.", "author" : "Camille Alice"},
+        {"quote" : "Never seem wiser, nor more learned, than the people you are with. Wear your learning, like your watch, in a private pocket: and do not merely pull it out and strike it; merely to show that you have one.", "author" : "Lord Chesterfield"},
+        {"quote" : "Life often teaches us through our wrong turns and missed possibilities.", "author" : "Anne Wilson Schaef"},
+    ]
+];
